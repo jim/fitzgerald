a40a27ce50d80273fc185e59b3ca6834ebc7956f
tyoro <orz@tyo.ro>
2011-12-08 13:11:59 +0900

    can use a '_' in the pattern parameter.

--- lib/fitzgerald.php
+++ lib/fitzgerald.php
@@ -46,7 +46,7 @@
                 $paramNames = array();
                 $paramValues = array();
 
-                preg_match_all('@:([a-zA-Z]+)@', $url, $paramNames, PREG_PATTERN_ORDER);                    // get param names
+                preg_match_all('@:([a-zA-Z_]+)@', $url, $paramNames, PREG_PATTERN_ORDER);                    // get param names
                 $paramNames = $paramNames[1];                                                               // we want the set of matches
                 $regexedUrl = preg_replace_callback('@:[a-zA-Z_\-]+@', array($this, 'regexValue'), $url);     // replace param with regex capture
                 if (preg_match('@^' . $regexedUrl . '$@', $requestUri, $paramValues)){                      // determine match and get param values
